36b62cdfa09546c142550238d3ab54adab168180
adiiwill <gazdagadam02@gmail.com>
2023-02-18 14:36:00 +0100

    404 addition

---
+++ 404.js
@@ -0,0 +1,9 @@
+function toHU() {
+    document.getElementById("text").innerHTML = "Itt nincs semmi...<br>Ha látni szeretnéd a projektjeimet, kattints a lenti gombra."
+    document.getElementById("btn").innerHTML = "Projektjeim"
+}
+
+function toEN() {
+    document.getElementById("text").innerHTML = "There is nothing here...<br>If you'd like to see my projects please press the button below."
+    document.getElementById("btn").innerHTML = "My projects"
+}
