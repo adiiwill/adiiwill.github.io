50aeb7cb4a54f614a22f39f27bf49b90b988b203
Adam <gazdagadam02@gmail.com>
2023-11-22 10:14:19 +0100

    updated weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -4,7 +4,11 @@ export const Weather = async (args) => {
   if (args) {
     let response = await fetch(`https://wttr.in/${args[0]}?ATm&format=3`);
     let data = await response.text();
-    Log(`<pre>${response.ok ? data : "City not found..."}</pre>`);
+    Log(
+      `<pre>${
+        response.ok ? `${data}Powered by wttr.in` : "City not found..."
+      }</pre>`
+    );
   } else {
     Log(`<pre>Invalid syntax
 
