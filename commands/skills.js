e83d7ad18be8fedf581c3f155ab1efa2ba06c0cf
Adee71 <48097544+adiiwill@users.noreply.github.com>
2024-04-30 20:16:12 +0200

    Update colors of skills.js

--- commands/skills.js
+++ commands/skills.js
@@ -3,24 +3,24 @@ import { Log } from "./log.js";
 export const Skills = () => {
   Log(`<pre>Things I've got experience in:
   
-  ╔══════& <span style="color: var(--cmdcolor)">Web</span>
+  ╔══════& <span style="color: var(--cmdhighlight)">Web</span>
   ╠══# <span style="color: var(--cmdcolor)">JavaScript /w node_js</span>
   ╠══# <span style="color: var(--cmdcolor)">React</span>
   ╠══# <span style="color: var(--cmdcolor)">TypeScript</span>
   ╠══# <span style="color: var(--cmdcolor)">PHP</span>
   ╠══# <span style="color: var(--cmdcolor)">UX/UI</span>
-  ╠══════& <span style="color: var(--cmdcolor)">Programming languages</span>
+  ╠══════& <span style="color: var(--cmdhighlight)">Programming languages</span>
   ╠══# <span style="color: var(--cmdcolor)">C#</span>
   ╠══# <span style="color: var(--cmdcolor)">C++</span>
   ╠══# <span style="color: var(--cmdcolor)">C</span>
   ╠══# <span style="color: var(--cmdcolor)">Java</span>
   ╠══# <span style="color: var(--cmdcolor)">Python</span>
-  ╠══════& <span style="color: var(--cmdcolor)">Databases</span>
+  ╠══════& <span style="color: var(--cmdhighlight)">Databases</span>
   ╠══# <span style="color: var(--cmdcolor)">MySQL</span>
   ╠══# <span style="color: var(--cmdcolor)">MongoDB</span>
   ╚══# <span style="color: var(--cmdcolor)">Database integrations /w php, node_js</span>
 
-  ╔══════& <span style="color: var(--cmdcolor)">Gained knowledge from:</span>
+  ╔══════& <span style="color: var(--cmdhighlight)">Gained knowledge from:</span>
   ╠══# <span style="color: var(--highlight)">[CS50's Introduction to Computer Science by Hardvard]</span>
   ╠══# <span style="color: var(--highlight)">[CS50's Introduction to Programming with Python by Hardvard]</span>
   ╠══# <span style="color: var(--highlight)">[Website: freeCodeCamp]</span>
