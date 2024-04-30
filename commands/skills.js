3ca02d55e4bc1db51630c6f23485e760b7037996
Adee71 <48097544+adiiwill@users.noreply.github.com>
2024-04-30 19:47:56 +0200

    Update skills.js

--- commands/skills.js
+++ commands/skills.js
@@ -1,25 +1,31 @@
 import { Log } from "./log.js";
 
 export const Skills = () => {
-  Log(`<pre>My skills:
+  Log(`<pre>Things I've got experience in:
   
-  ╔══════& <span style="color: var(--cmdcolor)">JavaScipt</span>  - [========..] 80%
-  ╠══# <span style="color: var(--cmdcolor)">Node_js</span> - [=======...] 75%
-  ╠══# <span style="color: var(--cmdcolor)">React</span>   - [====......] 40%
-  ╠══# <span style="color: var(--cmdcolor)">Svelte</span>  - [=.........] 15%
-  ╠══════& <span style="color: var(--cmdcolor)">.NET & OOP</span> - [=======...] 70%
-  ╠══# <span style="color: var(--cmdcolor)">C#</span>      - [========..] 85%
-  ╠══# <span style="color: var(--cmdcolor)">Java</span>    - [===.......] 30%
-  ╠══# <span style="color: var(--cmdcolor)">C++</span>     - [==........] 20%
-  ╠══# <span style="color: var(--cmdcolor)">Python</span>  - [=======...] 75%
-  ╠══════& <span style="color: var(--cmdcolor)">REST API</span>   - [========..] 80%
-  ╠══# <span style="color: var(--cmdcolor)">MySQL</span>     - [======....] 65%
-  ╠══# <span style="color: var(--cmdcolor)">MongoDB</span>   - [=======...] 70%
-  ╚══# <span style="color: var(--cmdcolor)">npm_tools</span> - [=====.....] 55%
+  ╔══════& <span style="color: var(--cmdcolor)">Web</span>
+  ╠══# <span style="color: var(--cmdcolor)">JavaScript /w node_js</span>
+  ╠══# <span style="color: var(--cmdcolor)">React</span>
+  ╠══# <span style="color: var(--cmdcolor)">TypeScript</span>
+  ╠══# <span style="color: var(--cmdcolor)">PHP</span>
+  ╠══# <span style="color: var(--cmdcolor)">UX/UI</span>
+  ╠══════& <span style="color: var(--cmdcolor)">Programming languages</span>
+  ╠══# <span style="color: var(--cmdcolor)">C#</span>
+  ╠══# <span style="color: var(--cmdcolor)">C++</span>
+  ╠══# <span style="color: var(--cmdcolor)">C</span>
+  ╠══# <span style="color: var(--cmdcolor)">Java</span>
+  ╠══# <span style="color: var(--cmdcolor)">Python</span>
+  ╠══════& <span style="color: var(--cmdcolor)">Databases</span>
+  ╠══# <span style="color: var(--cmdcolor)">MySQL</span>
+  ╠══# <span style="color: var(--cmdcolor)">MongoDB</span>
+  ╚══# <span style="color: var(--cmdcolor)">Database integrations /w php, node_js</span>
 
-  ╔══════& <span style="color: var(--cmdcolor)">Completed courses</span>
+  ╔══════& <span style="color: var(--cmdcolor)">Gained knowledge from:</span>
   ╠══# <span style="color: var(--highlight)">[CS50's Introduction to Computer Science by Hardvard]</span>
-  ╚══# <span style="color: var(--highlight)">[CS50's Introduction to Programming with Python by Hardvard]</span>
+  ╠══# <span style="color: var(--highlight)">[CS50's Introduction to Programming with Python by Hardvard]</span>
+  ╠══# <span style="color: var(--highlight)">[Website: freeCodeCamp]</span>
+  ╠══# <span style="color: var(--highlight)">[Website: Exercism]</span>
+  ╚══# <span style="color: var(--highlight)">[Website: W3Schools]</span>
 
   </pre>`);
 };
