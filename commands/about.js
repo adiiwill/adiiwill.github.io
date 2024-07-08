d78ccdadc01cf9bc29762c2819117407dcffad09
Adee71 <48097544+adiiwill@users.noreply.github.com>
2024-07-08 21:28:36 +0200

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -1,11 +1,10 @@
 import { Log } from "./log.js";
 
 export const About = () => {
-  Log(`<pre># Hello, I'm adiiwill.
-  I'm a developer who makes fun projects like this one. <span style="color: var(--highlight)">Web development</span> is not the only thing I do, but I adore <span style="color: var(--highlight)">JavaScript</span> because I feel more capable and efficient within it. I'm currently learning component-based frameworks like <span style="color: var(--highlight)">React</span> and <span style="color: var(--highlight)">Svelte</span>. I've also had experience doing <span style="color: var(--highlight)">API</span> backend stuff, so I'm familiar with most of the concepts. I'm not a professional, but I aspire to be one one day.
-  
-  I'm also <span style="color: var(--highlight)">familiar with .NET and OOP</span> programming languages like <span style="color: var(--highlight)">C# or Java and Python</span>. I'm open to new things and love to do something that I haven't done before. I love working out, and being active. I write as a hobby and when I have some free time, I try to spend it outdoors. I love hiking and being in the mountains. My goal is to become a better me every day, and to make the most out of my ideas.
-  
-  Feel free to contact me. I'm open to do backend jobs as a career starter! You can check my experience with the <span style="color: var(--cmdcolor)">[skills]</span> command. Type <span style="color: var(--cmdcolor)">[contact]</span> to see my socials.</pre>
+  Log(`<pre># Hello, I'm adiiwill
+I'm a developer who enjoys creating engaging projects like this portfolio. My experience spans various programming languages including C#, Java, and Python. Recently, I've been exploring component-based frameworks and diving into API backend development.
+While web development is a key focus, I'm versatile and always eager to learn new technologies. I'm not a seasoned professional yet, but I'm dedicated to growing my skills every day.
+Outside of coding, I'm passionate about hiking, creative writing, and exploring the outdoors. I believe in pushing myself to try new things and continuously improve.
+I'm open to new opportunities, particularly in backend development as I look to advance my career. Feel free to check out my skills using the <span style="color: var(--cmdcolor)">[skills]</span> command, or reach out via the <span style="color: var(--cmdcolor)">[contact]</span> command. I'm excited about the possibility of collaborating on innovative projects!</pre>
   `);
 };
